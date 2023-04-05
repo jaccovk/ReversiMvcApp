dedcb914b76e588c558f9654019f3f9157ad9131
jaccovk <s1158428@student.windesheim.nl>
2023-04-05 15:45:58 +0200

    replaced url

--- ReversiMvcApp/Settings.cs
+++ ReversiMvcApp/Settings.cs
@@ -3,9 +3,9 @@
     public static class Settings
     {
         private static readonly bool _isProduction = false;
-        public static string Url => _isProduction ? 
-            "https://localhost:44326/api/Spel" : 
-            "http://localhost:3000/api/Spel";
+        public static string Url => _isProduction ?
+             "http://localhost:3000/api/Spel" :
+            "https://localhost:44326/api/Spel";
 
     }
 }
